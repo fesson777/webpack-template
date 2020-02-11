613cfe5fb5cf3991fde2d8484eae28fad78babf3
fesson777 <cccp0110@gmail.com>
2020-02-11 10:36:36 +0300

    second commit 01:12:30

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Post from './Post'
 import json from './assets/json.json'
 import './styles/styles.css'
+import WebpackLogo from './assets/webpack-logo.png'
 
-const post = new Post('Webpack title')
+const post = new Post('Webpack title', WebpackLogo)
 
 console.log("Post to String: ", post.toString())
 
